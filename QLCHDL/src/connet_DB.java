624449f36a4ef65e6875c0cbcdef37640755c0e2
atjpa <caoducan351@gmail.com>
2022-03-21 12:16:29 +0700

    ver1

---
+++ QLCHDL/src/connet_DB.java
@@ -0,0 +1,16 @@
+import java.sql.*;
+
+public class connet_DB {
+	public static void main(String args[]) {
+		Connection conn = null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost/qlchdl?" + "user=root");
+			System.out.println(conn);
+			System.out.println("Noi ket thanh cong");
+		} catch (Exception ex) { // xử lý ngoại lệ nếu có
+			System.out.println("Noi ket khong thanh cong");
+			ex.printStackTrace();
+		}
+	}
+}
